e53c5e9fed20be97253d65439533c70a969a7c0b
2300033836 <kallepallisushmasri@gmail.com>
2025-09-10 20:55:42 +0530

    Set Vite build output to 'build'

--- FRONTEND/Demoreactapp/vite.config.js
+++ FRONTEND/Demoreactapp/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/ReactSpringBootCRUD/'
+  base: '/ReactSpringBootCRUD/',
+  build: {
+    outDir: 'build', // output folder changed from 'dist' to 'build'
+  },
 })
